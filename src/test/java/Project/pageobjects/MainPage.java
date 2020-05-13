77a6c78b1c45bb29d9e9277e6d15254f5454ac40
Maria <mstrebulyaeva@gmail.com>
2020-05-13 14:31:30 +0300

    Add new PageObjects for main page and Top menu

---
+++ src/test/java/Project/pageobjects/MainPage.java
@@ -0,0 +1,26 @@
+package Project.pageobjects;
+
+import Project.Session;
+import Project.helpers.TestHelper;
+import config.ProjectConfig;
+import io.qameta.allure.Step;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MainPage {
+    private static Logger Log = LogManager.getLogger(MainPage.class);
+
+    @Autowired
+    ProjectConfig cfg;
+
+    By loc_upc_event = By.xpath("//a[@class='evnt-filtered-link'][contains(text(), 'Upcoming events')]");
+
+    @Step
+    public void openMainPage (Session session) throws Exception {
+        TestHelper.getURL(session.getWebDriver(), cfg.hostname());
+        TestHelper.isPageLoad(session.getWaiter(), loc_upc_event, "Main Menu");
+    }
+
+}
