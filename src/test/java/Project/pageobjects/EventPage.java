14e058dc4bdcf38d0b71b6c7caedf627a63584c5
Maria <mstrebulyaeva@gmail.com>
2020-05-17 01:53:46 +0300

    Implementation of the 2-nd TestCases

--- src/test/java/Project/pageobjects/EventPage.java
+++ src/test/java/Project/pageobjects/EventPage.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -22,7 +23,11 @@ public class EventPage {
     By loc_events_cards = By.cssSelector(".evnt-cards-container .evnt-event-card");
     By loc_card_sections = By.cssSelector(".evnt-card-wrapper>div");
     By loc_head_sections = By.cssSelector(".evnt-event-details-table>div");
+    By loc_body_sections = By.cssSelector(".evnt-card-cell div");
     By loc_card_elem_text = By.cssSelector("span");
+    By loc_card_dates_elem_text = By.cssSelector(".evnt-dates-cell.dates span");
+    By loc_speaker_data = By.cssSelector(".speakers div[data-name]");
+    By loc_loader = By.cssSelector(".evnt-global-loader");
 
     @Step ("wait Until Event Page is Loaded")
     public EventPage waitUntilLoad (Session session){
@@ -47,14 +52,18 @@ public class EventPage {
        return session.getWebDriver().findElements(loc_events_cards).size();
     }
 
+    @Step ("Check Event Card Сorrectness By Num ")
     public boolean checkEventCardСorrectnessByNum (Session session, int num) {
-        boolean result = false;
+        boolean result = true;
         WebElement cardHead;
         WebElement cardBody;
         WebElement cardLocation;
         WebElement cardLang;
+        WebElement cardEventName;
+        WebElement cardDates;
 
         WebElement card = session.getWebDriver().findElements(loc_events_cards).get(num);
+        Log.info("Check top level structure of -" + num + "- card");
         if (card.findElements(loc_card_sections).get(0).getAttribute("class").equals("evnt-card-heading")){
             cardHead = card.findElements(loc_card_sections).get(0);
         }
@@ -69,6 +78,16 @@ public class EventPage {
             Log.error("Card-body is not the second element at the card");
             return false;
         }
+        if (card.findElements(loc_card_sections).get(2).getAttribute("class").equals("evnt-card-body")){
+            cardBody = card.findElements(loc_card_sections).get(1);
+        }
+        else {
+            Log.error("Card-body is not the second element at the card");
+            return false;
+        }
+        Log.info("Top level structure: heading and body of -" + num + "- card is successfully");
+
+        Log.info("Check heading structure of -" + num + "- card");
         if (cardHead.findElements(loc_head_sections).get(0).getAttribute("class").equals("evnt-details-cell online-cell") ||
             cardHead.findElements(loc_head_sections).get(0).getAttribute("class").equals("evnt-details-cell location-cell"))
         {
@@ -76,21 +95,116 @@ public class EventPage {
             if (cardLocation.findElement(loc_card_elem_text).getText().isEmpty())
             {
                 Log.error("Card-Location is empty");
-                return false;
+                result = false;
             }
         }
         else {
             Log.error("Card-location is not the first element of the heading");
-            return false;
+            result = false;
         }
         if (cardHead.findElements(loc_head_sections).get(1).getAttribute("class").equals("evnt-details-cell language-cell")){
             cardLang = card.findElements(loc_card_sections).get(1);
+            if (cardLang.findElement(loc_card_elem_text).getText().isEmpty())
+            {
+                Log.error("Card-Language is empty");
+                result = false;
+            }
         }
         else {
             Log.error("Card-lang is not the second element of the heading");
+            result = false;
+        }
+        Log.info("Heading structure of -" + num + "- card is checked with result: " + result);
+
+
+        Log.info("Check body structure of -" + num + "- card");
+        if (cardBody.findElements(loc_body_sections).get(0).getAttribute("class").equals("evnt-event-name"))
+        {
+            cardEventName = cardHead.findElements(loc_head_sections).get(0);
+            if (cardEventName.findElement(loc_card_elem_text).getText().isEmpty())
+            {
+                Log.error("Card EventName is empty");
+                result = false;
+            }
+        }
+        else {
+            Log.error("Event name is not the first element of the body");
+            result = false;
+        }
+        if (cardBody.findElements(loc_head_sections).get(1).getAttribute("class").equals("evnt-event-dates")){
+            cardDates = card.findElements(loc_card_sections).get(1);
+            if (cardDates.findElements(loc_card_dates_elem_text).get(0).getAttribute("class").equals("date"))
+            {
+                if (cardDates.findElements(loc_card_dates_elem_text).get(0).getText().isEmpty())
+                {
+                    Log.error("Card-Date is empty");
+                    result = false;
+                }
+            }
+            else{
+                Log.error("Card Date is not the second element of the body");
+                result = false;
+            }
+
+            if (cardDates.findElements(loc_card_dates_elem_text).get(1).getText().isEmpty()) // Registration info
+            {
+                Log.error("Registration info is empty");
+                result = false;
+            }
+        }
+        else {
+            Log.error("Card-dates section is absent in the body");
+            result = false;
+        }
+        Log.info("Body structure of -" + num + "- card is checked with result: " + result);
+
+        Log.info("Check speakers for -" + num + "- card");
+        try{
+            card.findElement(loc_speaker_data);
+        }
+        catch (Exception e){
+            Log.error("Speakers are not found");
+            result = false;
+        }
+        Log.info("Speakers of -" + num + "- card are checked with result: " + result);
+        Log.info("Checking of -" + num + "- card is completed with result: " + result);
+
+        return result;
+    }
+
+
+    @Step ("Check All Upcoming Event Cards")
+    public boolean checkAllUpcomingEventCards (Session session) {
+        boolean result = true;
+
+        int eventsAmount = session.getWebDriver().findElements(loc_events_cards).size();
+
+        if (eventsAmount == 0) {
+            Log.error("Upcoming Events are not found");
             return false;
         }
 
+        Log.info("Start the Event cards checking");
+        for (int i=0; i<eventsAmount; i++) {
+            if(checkEventCardСorrectnessByNum (session, i) == false) {
+                result = false;
+            }
+        }
+        Log.info("All Upcoming Event Cards checking is completed with result: " + result);
+        return result;
     }
+
+
+    @Step ("Wait the loader")
+    public void waitLoader (Session session) {
+        try{
+            session.getWaiter().until(ExpectedConditions.invisibilityOfElementLocated(loc_loader));
+            Log.info("Events are loaded");
+        } catch (Exception e) {
+            Log.error("Events are not loaded", e);
+            throw e;
+        }
+    }
+
 }
 
